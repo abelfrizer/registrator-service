3006fc9418237d4e854a4862d9f7ebc0e5355c7c
Abel Mapharisa <abelm@wyzcrew.com>
2020-06-23 21:48:16 +0200

    Created a registrator-service project

---
+++ src/test/java/net/iqbusiness/abelm/app/registrator/RegistratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.iqbusiness.abelm.app.registrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
